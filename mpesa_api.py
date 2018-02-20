cdbd66805528aa7aab49581266d08031c027dd08
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2018-02-20 15:18:30 +0300

    Add test file and requiremnts file

---
+++ mpesa_api.py
@@ -0,0 +1,5 @@
+class Mpesa(object):
+    def __init__(self):
+        pass
+    def confirm_transaction(self):
+        pass
---
+++ mpesa_api_test.py
@@ -0,0 +1,10 @@
+import unittest
+from 
+class MpesaTestCase(unittest.TestCase):
+    
+    def test_something(self):
+        self.assertEqual(1,1, '1 compared to 1 should be equal')
+
+
+if __name__ == '__main__':
+    unittest.main()
